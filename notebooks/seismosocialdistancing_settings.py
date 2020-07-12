899cb58b98fe9821c1d928b49616a68c421e00da
Louis Moresi <louis.moresi@anu.edu.au>
2020-07-12 19:46:44 +1000

    Update seismosocialdistancing_settings.py

--- notebooks/seismosocialdistancing_settings.py
+++ notebooks/seismosocialdistancing_settings.py
@@ -32,10 +32,12 @@ data_provider = "http://auspass.edu.au:8080"
 logo = None # 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Logo_SED_2014.png/220px-Logo_SED_2014.png'
 bans = {"2019-12-20 00:00":"Start of School Summer Holiday",
         "2020-01-28 00:00":"End of School Summer Holiday",
-        "2020-03-18 00:00":'No Large Gatherings', 
+        "2020-03-18 00:00":'No Large Gatherings',
         "2020-03-25 12:00":'Restaurants/Bars/Schools closed',
-        "2020-04-10 12:00":'Easter long weekend'}
-
+        "2020-04-10 00:00":"Easter", 
+        "2020-05-18 00:00":"Schools open (VIC/NSW)", 
+        "2020-07-07 00:00":"Restrictions re-imposed (VIC)"
+        }
 
 reference = {"start": "2020-01-28 00:00",
               "end":  "2020-03-18 00:00"}
